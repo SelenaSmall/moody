32d1b3bd7a933f8cec1d35f360754605f93c9893
selenasmall <selenawiththetattoo@gmail.com>
2020-07-03 13:38:00 +1000

    First passing integration test
    Add new test: spec/features/app_exists_spec.rb
    Add gem: chromedriver-helper
    Configure Capybara
    
    $ bundle exec rspec

---
+++ spec/features/app_exists_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+feature 'App exists', js: true do
+  scenario 'app exists' do
+    When 'user visits the app' do
+      visit('/')
+    end
+
+    Then 'user sees they are on rails' do
+      wait_for { page }.to have_content("Hello React!")
+    end
+  end
+end
